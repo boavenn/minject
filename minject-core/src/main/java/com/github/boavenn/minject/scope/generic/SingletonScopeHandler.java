f087c1ca8165bb303a5ed2288e3af7903c47f59b
Witold Brzozowski <brzozowskyy@gmail.com>
2021-10-05 22:43:54 +0200

    Make `SingletonScopeHandler` thread-safe

--- minject-core/src/main/java/com/github/boavenn/minject/scope/generic/SingletonScopeHandler.java
+++ minject-core/src/main/java/com/github/boavenn/minject/scope/generic/SingletonScopeHandler.java
@@ -5,19 +5,16 @@ import com.github.boavenn.minject.scope.ScopeHandler;
 import lombok.NoArgsConstructor;
 
 import javax.inject.Provider;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @NoArgsConstructor(staticName = "empty")
 public class SingletonScopeHandler implements ScopeHandler {
-    private final Map<ClassKey<?>, Object> singletons = new HashMap<>();
+    private final Map<ClassKey<?>, Object> singletons = new ConcurrentHashMap<>();
 
     @Override
     @SuppressWarnings("unchecked")
     public <T> Provider<? extends T> scopeProvider(ClassKey<T> classKey, Provider<? extends T> unscopedProvider) {
-        return () -> {
-            singletons.computeIfAbsent(classKey, key -> unscopedProvider.get());
-            return (T) singletons.get(classKey);
-        };
+        return () -> (T) singletons.computeIfAbsent(classKey, key -> unscopedProvider.get());
     }
 }
